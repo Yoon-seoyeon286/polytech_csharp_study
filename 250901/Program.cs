cb342ff46e58047b3d69222b89684f46beea0b95
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-01 11:30:31 +0900

    feat : 10.5는 float 형식이기에 int의 범위를 벗어나서 예외가 발생하므로 10.5를 10으로 변경

--- 250901/Program.cs
+++ 250901/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var numString = "10.5";
+        string numString = "10";
         int num  = int.Parse(numString);
         Console.WriteLine(num);
     }
