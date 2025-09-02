da916073b0ac0ea5f7119bf880f0644b6f832bfd
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-02 12:33:19 +0900

    feat : 직렬화 역직렬화

--- 250902/Program.cs
+++ 250902/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace _250902;
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace _250902;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Employee employee = new Employee("홍길동", 41);
+        string jsonString = JsonSerializer.Serialize(employee);
+        System.IO.File.WriteAllText("company.json", jsonString);
+        Employee? loadedEmployee = JsonSerializer.Deserialize<Employee>(jsonString);
+
     }
 }
