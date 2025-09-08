92f0669ba32c4ae86f31e593c6042a6fa1f0a5f4
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-08 11:26:14 +0900

    feat : bird 3개 생성

--- 250908/Program.cs
+++ 250908/Program.cs
@@ -2,6 +2,23 @@
 
 class Program
 {
+    public static async Task<string> FirstBird()
+    {
+        await Task.Delay(1000);
+        return "꾸우우";
+    }
+
+    public static async Task<string> SecondBird()
+    {
+        await Task.Delay(2000);
+        return "까악";
+    }
+
+    public static async Task<string> ThirdBird()
+    {
+        await Task.Delay(3000);
+        return "짹쨱쨲ㄱ잭";
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
