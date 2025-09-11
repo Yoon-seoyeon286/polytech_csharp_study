2ffd8e369c5a335e7980610f05f0ddb31008dc4b
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-11 15:55:26 +0900

    feat : Test code3 Success

--- 250911.Tests/UnitTest1.cs
+++ 250911.Tests/UnitTest1.cs
@@ -36,4 +36,18 @@ public class Tests
              Assert.IsTrue(checkItem.Any(items=> items.Name=="Potion"));
              
          }
+
+         [Test]
+         public async Task Test3()
+         {
+             MockItemDataSource _mockItemDataSource = new MockItemDataSource();
+             InventoryRepository _inventory = new InventoryRepository(_mockItemDataSource, 10,20);
+             var checkItem = await _inventory.GetItemsAsync();
+             
+            Item swordItem = new Item(10, "Sword", 1);
+            checkItem.Add(swordItem);
+             
+             Assert.IsTrue(checkItem.Any(item =>item.Count==2));
+             
+         }
 }
