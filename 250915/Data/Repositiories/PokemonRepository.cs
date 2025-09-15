2c0b6ab89b6141b7491ee3b6fe69144c97329103
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-15 13:22:49 +0900

    feat : PokemonRepository class

--- 250915/Data/Repositiories/PokemonRepository.cs
+++ 250915/Data/Repositiories/PokemonRepository.cs
@@ -1,6 +1,22 @@
+using _250915.Data.DataSources;
+using _250915.Data.Models;
+
 namespace _250915.Data.Repositiories;
 
-public class PokemonRepository
+public class PokemonRepository : IPokemonRepsoitory
 {
-    
+    private IPokemonApiDataSource<Pokemon> _DataSource;
+
+    public PokemonRepository(IPokemonApiDataSource<Pokemon> dataSource)
+    {
+        _DataSource = dataSource;
+    }
+
+    public async Task<Pokemon?> GetPokemonByNameAsync(string pokemonName)
+    {
+        var response = await _DataSource.GetPokemonAsync(pokemonName);
+        return response.Body;
+    }
+
+
 }
