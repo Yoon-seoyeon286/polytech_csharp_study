2a49207a05ee0f5c0c14d99877505c3a1fa21d73
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-15 13:10:32 +0900

    feat : PokemonApiDataSource class

--- 250915/Data/DataSources/PokemonApiDataSource.cs
+++ 250915/Data/DataSources/PokemonApiDataSource.cs
@@ -1,6 +1,32 @@
+using _250915.Data.Models;
+using Newtonsoft.Json;
+
 namespace _250915.Data.DataSources;
 
-public class PokemonApiDataSource
+public class PokemonApiDataSource : IPokemonApiDataSource<Pokemon>
 {
+    private const string BaseUrl = "https://pokeapi.co/api/v2/pokemon";
+    private HttpClient _httpClient;
+
+    public PokemonApiDataSource(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<Response> GetPokemonAsync(string pokemonName)
+    {
+        var response = await _httpClient.GetAsync($"{BaseUrl}/{pokemonName}");
+        var jsonString = await response.Content.ReadAsStringAsync();
+
+        var headers = response.Headers.ToDictionary(
+            h => h.Key,
+            h => string.Join(", ", h.Value));
+
+        return new Response(
+            statusCode:(int)response.StatusCode,
+            headers: headers,
+            body: JsonConvert.DeserializeObject<Pokemon>(jsonString)
+        );
+
+    }
     
 }
