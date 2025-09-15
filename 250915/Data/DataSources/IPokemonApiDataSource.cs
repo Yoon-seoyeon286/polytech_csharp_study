3e4fe055458b1da525f296f2d84e0ab000dcafb9
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-15 12:49:49 +0900

    feat : IPokemonApiDataSource interface

--- 250915/Data/DataSources/IPokemonApiDataSource.cs
+++ 250915/Data/DataSources/IPokemonApiDataSource.cs
@@ -1,6 +1,7 @@
 namespace _250915.Data.DataSources;
 
-public interface IPokemonApiDataSource
+public interface IPokemonApiDataSource<T>
 {
+    public Task<Response> GetPokemonAsync(string pokemonName);
     
 }
