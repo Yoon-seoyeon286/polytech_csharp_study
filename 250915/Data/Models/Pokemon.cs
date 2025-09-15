33b2ea0786407bc17832b423be72455ea297ee6f
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-15 11:27:50 +0900

    feat : pokemon class complete

--- 250915/Data/Models/Pokemon.cs
+++ 250915/Data/Models/Pokemon.cs
@@ -10,6 +10,27 @@ public class Pokemon
         Id = id;
         Name = name;
     }
-    
-    
+
+    protected bool Equals(Pokemon other)
+    {
+        return Id == other.Id && Name == other.Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if(obj is null) return false;
+        if(ReferenceEquals(this, obj)) return true;
+        if(obj.GetType() != GetType()) return false;
+        return Equals((Pokemon)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name);
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}";
+    }
 }
