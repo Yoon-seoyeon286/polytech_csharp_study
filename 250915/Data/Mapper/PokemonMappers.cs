b9f11153f943d458e9be65ec8a44c33a7276fbe0
Yoon-seoyeon286 <panopticon0847@gmail.com>
2025-09-15 11:45:48 +0900

    feat : Mapper 설명 추가

--- 250915/Data/Mapper/PokemonMappers.cs
+++ 250915/Data/Mapper/PokemonMappers.cs
@@ -1,13 +1,15 @@
 namespace _250915.Data.Mapper;
 
-public static class PokemonMappers
+public static class PokemonMappers //한 객체를 다른 객체로 변환해주는 역할
 {
-    public static Models.Pokemon ToModel(this PokemonDto dto)
+    //매퍼는 코드에서 객체(PokemonDto)의 데이터를 다른 객체(Models.Pokemon)로 변환
+    public static Models.Pokemon ToModel(this PokemonDto dto) //확장 메소드
     {
-        return new Models.Pokemon
+        return new Models.Pokemon //실제 변환 수행
         (
             id: dto.Id,
             name: dto.Name
+            //로운 Models.Pokemon 객체를 생성
         );
     }
     
